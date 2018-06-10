3035c11dee4d9ac589274e59649158f30fff1f7c
gaiaroyeca <40084464+gaiaroyeca@users.noreply.github.com>
2018-06-10 20:28:43 +0800

    Add files via upload

---
+++ exercises01.py
@@ -0,0 +1,5 @@
+# TinderaBot
+total_bill = float (input ('Hi! How much is your total bill?'))
+pmt_amt = float (input ('How much will your payment be?'))
+tot_change = pmt_amt - total_bill
+print ("Your change is {}. Thank you for choosing Aling Nena's Sari-Sari Store! Please come back again.".format (tot_change))
